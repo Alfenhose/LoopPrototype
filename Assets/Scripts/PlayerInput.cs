0a2e56992260f64fd36c509ff4d64d5d04c8a0ce
Mikkel Balslev <mibal@itu.dk>
2017-10-03 10:36:55 +0200

    playerInput interface

--- Assets/Scripts/PlayerInput.cs
+++ Assets/Scripts/PlayerInput.cs
@@ -1,14 +1,34 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.Events;
 
 public class PlayerInput : MonoBehaviour {
 
-    public float Horizontal;
-    public float Vertical;
-    
-	// Use this for initialization
-	void Start () {
+    static PlayerInput Instance;
+    public float Player1_Horizontal;
+    public float Player1_Vertical;
+    public float Player2_Horizontal;
+    public float Player2_Vertical;
+    public UnityEvent Player1_Jump;
+    public UnityEvent Player2_Jump;
+    public UnityEvent Player1_Use;
+    public UnityEvent Player2_Use;
+
+    private void Awake()
+    {
+        if (Instance)
+        {
+            DestroyImmediate(gameObject);
+        }
+        else
+        {
+            Instance = this;
+            DontDestroyOnLoad(gameObject);
+        }
+    }
+    // Use this for initialization
+    void Start () {
 		
 	}
 	
