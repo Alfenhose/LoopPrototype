984e62b2a6ead4fc80fc0db9d8ed8db0aea4a820
Mikkel Balslev <mibal@itu.dk>
2017-10-03 11:05:35 +0200

    collision

--- Assets/Scripts/PlayerIsometric.cs
+++ Assets/Scripts/PlayerIsometric.cs
@@ -1,17 +1,29 @@
-﻿using System.Collections;
+﻿using System;
+using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
 public class PlayerIsometric : MonoBehaviour {
 
     private Rigidbody2D rigidbody;
-	// Use this for initialization
-	void Start () {
+    public float horizontalSpeed = 5;
+    public float verticalSpeed = 5;
+    // Use this for initialization
+    void Start () {
         rigidbody = GetComponent<Rigidbody2D>();
 	}
 	
 	// Update is called once per frame
 	void Update () {
-        rigidbody.velocity = new Vector2(PlayerInput.Instance.Horizontal, PlayerInput.Instance.Vertical);
-	}
+        rigidbody.velocity = new Vector2(PlayerInput.Instance.Horizontal * horizontalSpeed,
+                                         PlayerInput.Instance.Vertical * verticalSpeed);
+        SetDepth();
+
+    }
+
+    private void SetDepth()
+    {
+        float newZ = transform.position.y/100;
+        transform.position = new Vector3(transform.position.x, transform.position.y, newZ);
+    }
 }
