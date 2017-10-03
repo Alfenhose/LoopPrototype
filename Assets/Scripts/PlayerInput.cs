35f930e3fca9696e908e76883b40c83142f587cc
Mikkel Balslev <mibal@itu.dk>
2017-10-03 10:38:10 +0200

    forgot there was only one player

--- Assets/Scripts/PlayerInput.cs
+++ Assets/Scripts/PlayerInput.cs
@@ -6,14 +6,10 @@ using UnityEngine.Events;
 public class PlayerInput : MonoBehaviour {
 
     static PlayerInput Instance;
-    public float Player1_Horizontal;
-    public float Player1_Vertical;
-    public float Player2_Horizontal;
-    public float Player2_Vertical;
-    public UnityEvent Player1_Jump;
-    public UnityEvent Player2_Jump;
-    public UnityEvent Player1_Use;
-    public UnityEvent Player2_Use;
+    public float Horizontal;
+    public float Vertical;
+    public UnityEvent Jump;
+    public UnityEvent Use;
 
     private void Awake()
     {
