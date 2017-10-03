e61ae94e510ac87c03bd14d307db6d5852b5a788
Astrinaar <7h380x@gmail.com>
2017-10-03 11:36:28 +0200

    Move horizontally

--- Assets/Scripts/PlayerController2D.cs
+++ Assets/Scripts/PlayerController2D.cs
@@ -6,7 +6,9 @@ public class PlayerController2D : MonoBehaviour {
 
     private Rigidbody2D rBody;
     private bool canJump = true;
-    public float jumpHeight;
+    public float jumpHeight = 10;
+    public float moveSpeed = 10;
+    private bool moving = false;
 
 	// Use this for initialization
 	void Start () {
@@ -16,7 +18,17 @@ public class PlayerController2D : MonoBehaviour {
 	
 	// Update is called once per frame
 	void Update () {
-	}
+        if (PlayerInput.Instance.Horizontal > 0)
+        {
+            MoveRight();
+        } else if (PlayerInput.Instance.Horizontal < 0)
+        {
+            MoveLeft();
+        } else if (canJump)
+        {
+            rBody.velocity = new Vector2(0, rBody.velocity.y);
+        }
+    }
 
     private void Jump()
     {
@@ -27,9 +39,37 @@ public class PlayerController2D : MonoBehaviour {
         }
     }
 
-    private void moveHorizontally()
+    private void MoveRight()
     {
+        if (canJump)
+        {
+            rBody.velocity = new Vector2(moveSpeed, rBody.velocity.y);
+        }
+        else
+        {
+            if(rBody.velocity.x < moveSpeed)
+            {
+                rBody.velocity += new Vector2(moveSpeed * 1.5f * Time.deltaTime, 0);
+            }
+        }
+        
+        moving = true;
+    }
 
+    private void MoveLeft()
+    {
+        if (canJump)
+        {
+            rBody.velocity = new Vector2(-moveSpeed, rBody.velocity.y);
+        }
+        else
+        {
+            if (rBody.velocity.x > -moveSpeed)
+            {
+                rBody.velocity += new Vector2(-moveSpeed * 1.5f * Time.deltaTime, 0);
+            }
+        }
+        moving = true;
     }
 
     private void OnCollisionEnter2D(Collision2D collision)
