f29ea51de9624d68308351ace228cee94658fdab
Astrinaar <7h380x@gmail.com>
2017-10-07 20:27:22 +0200

    Loop 3 player controls done

--- Assets/Scripts/PlayerController2D.cs
+++ Assets/Scripts/PlayerController2D.cs
@@ -2,21 +2,50 @@
 using System.Collections.Generic;
 using UnityEngine;
 
-public class PlayerController2D : MonoBehaviour {
+public class PlayerController2D : MonoBehaviour
+{
 
     private Rigidbody2D rBody;
     private bool canJump = true;
-    public float jumpHeight;
+    public float jumpHeight = 20;
+    public float moveSpeed = 10;
+    public float acceleration = 75;
+    public float jumpSpeedModifier = 0.5f;
+    private PlayerInput pI;
+    
 
-	// Use this for initialization
-	void Start () {
+    // Use this for initialization
+    void Start()
+    {
         rBody = gameObject.GetComponent<Rigidbody2D>();
+        pI = PlayerInput.Instance;
         PlayerInput.Instance.Jump.AddListener(Jump);
     }
-	
-	// Update is called once per frame
-	void Update () {
-	}
+
+    // Update is called once per frame
+    void Update()
+    {
+        float jsm;
+        if (canJump)
+        {
+            jsm = 1;
+        } else
+        {
+            jsm = jumpSpeedModifier;
+        }
+        if (pI.Horizontal > 0)
+        {
+            rBody.velocity = new Vector2(Mathf.MoveTowards(rBody.velocity.x, moveSpeed, acceleration * Time.deltaTime * jsm), rBody.velocity.y);
+        }
+        else if (pI.Horizontal < 0)
+        {
+            rBody.velocity = new Vector2(Mathf.MoveTowards(rBody.velocity.x, -moveSpeed, acceleration * Time.deltaTime * jsm), rBody.velocity.y);
+        }
+        else
+        {
+            rBody.velocity = new Vector2(Mathf.MoveTowards(rBody.velocity.x, 0, acceleration * Time.deltaTime * jsm), rBody.velocity.y);
+        }
+    }
 
     private void Jump()
     {
@@ -27,18 +56,20 @@ public class PlayerController2D : MonoBehaviour {
         }
     }
 
-    private void moveHorizontally()
-    {
-
-    }
-
     private void OnCollisionEnter2D(Collision2D collision)
     {
-        canJump = true;
+        if (collision.gameObject.tag == "Ground")
+        {
+            canJump = true;
+        }
     }
 
     private void OnCollisionExit2D(Collision2D collision)
     {
-        canJump = false;
+        if (collision.gameObject.tag == "Ground")
+        {
+            canJump = false;
+        }
+
     }
 }
