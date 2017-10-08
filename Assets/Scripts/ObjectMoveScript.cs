71f8e120d55d32ef01dfde06919cee275a598b3d
Astrinaar <7h380x@gmail.com>
2017-10-09 00:49:26 +0200

    Loop 2 mostly done
    Lacks a win/lose condition, and could use a scrolling background of some form

---
+++ Assets/Scripts/ObjectMoveScript.cs
@@ -0,0 +1,71 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ObjectMoveScript : MonoBehaviour {
+
+    private Rigidbody2D rBody;
+    public float moveSpeed = -5;
+    public float fallingModifier = 5;
+    private bool knockedOver = false;
+    private Events events;
+    private bool fallen;
+    private bool gettingUp;
+
+    // Use this for initialization
+    void Start ()
+    {
+        rBody = gameObject.GetComponent<Rigidbody2D>();
+        events = Events.Instance;
+        //events.Fallen.AddListener(Fallen);
+        //events.GottenUp.AddListener(GottenUp);
+    }
+
+    // Update is called once per frame
+    void Update () {
+        Move();
+	}
+
+    private void Move()
+    {
+        //if (fallen)
+        //{
+        //    rBody.velocity = new Vector2(Mathf.MoveTowards(rBody.velocity.x, 0, fallingModifier * Time.deltaTime), rBody.velocity.y);
+        //} else if (gettingUp)
+        //{
+        //    rBody.velocity = new Vector2(Mathf.MoveTowards(rBody.velocity.x, moveSpeed, fallingModifier * Time.deltaTime), rBody.velocity.y);
+        //} else
+        //{
+        //    rBody.velocity = new Vector3(moveSpeed, 0, 0);
+        //}
+        rBody.velocity = new Vector2(Mathf.MoveTowards(rBody.velocity.x, events.scrollSpeed, fallingModifier * Time.deltaTime), rBody.velocity.y);
+    }
+
+    public void Collision()
+    {
+        if(gameObject.tag == "KnockOverAble" && !knockedOver)
+        {
+            gameObject.transform.up = new Vector3(1, 0, 0);
+            gameObject.transform.position += new Vector3(0, -0.19f , 0);
+            knockedOver = true;
+        }
+    }
+
+    private void Fallen()
+    {
+        fallen = true;
+    }
+
+    private void GottenUp()
+    {
+        fallen = false;
+        gettingUp = true;
+    }
+
+    IEnumerator GettingUp()
+    {
+        yield return new WaitForSeconds(1);
+        gettingUp = false;
+    }
+
+}
--- Assets/Scripts/PoliceChase.cs
+++ Assets/Scripts/PoliceChase.cs
@@ -2,31 +2,45 @@
 using System.Collections.Generic;
 using UnityEngine;
 
-public class PlayerControllerEscape : MonoBehaviour {
+public class PoliceChase : MonoBehaviour
+{
 
     private Rigidbody2D rBody;
     private bool canJump = true;
     public float jumpHeight = 20;
-    private PlayerInput pI;
     private Events events;
     private bool fallen = false;
-    
+    public float moveSpeed = 4;
+    public float fallingModifier = 5;
 
 
     // Use this for initialization
     void Start()
     {
         rBody = gameObject.GetComponent<Rigidbody2D>();
-        pI = PlayerInput.Instance;
-        pI.Jump.AddListener(Jump);
         events = Events.Instance;
-        //pI.Crouch.AddListener(Slide);
     }
 
     // Update is called once per frame
-    void Update () {
-		
-	}
+    void Update()
+    {
+        Move();
+    }
+
+    private void Move()
+    {
+        if (fallen)
+        {
+            rBody.velocity = new Vector2(events.scrollSpeed* 0.8f, rBody.velocity.y);
+            //rBody.velocity = new Vector2(Mathf.MoveTowards(rBody.velocity.x, events.scrollSpeed, fallingModifier * Time.deltaTime), rBody.velocity.y);
+        }
+        else
+        {
+            rBody.velocity = new Vector2(events.scrollSpeed/2 + moveSpeed, rBody.velocity.y);
+            //rBody.velocity = new Vector2(Mathf.MoveTowards(rBody.velocity.x, events.scrollSpeed + moveSpeed, fallingModifier * Time.deltaTime), rBody.velocity.y);
+        }
+
+    }
 
     private void Jump()
     {
@@ -37,23 +51,12 @@ public class PlayerControllerEscape : MonoBehaviour {
         }
     }
 
-    //private void Slide()
-    //{
-    //    if (canJump)
-    //    {
-    //        gameObject.transform.up = new Vector3(-1, 0, 0);
-    //        canJump = false;
-    //        StartCoroutine(slide());
-    //    }
-    //}
-
     IEnumerator GetUp(float time)
     {
         yield return new WaitForSeconds(time);
         canJump = true;
         fallen = false;
         gameObject.transform.up = new Vector3(0, 0, 0);
-        events.InvokeGottenUp();
     }
 
     private void OnCollisionEnter2D(Collision2D collision)
@@ -62,9 +65,10 @@ public class PlayerControllerEscape : MonoBehaviour {
         {
             if (fallen)
             {
-                StartCoroutine(GetUp(1f));
-            } else
-            canJump = true;
+                StartCoroutine(GetUp(0.6f));
+            }
+            else
+                canJump = true;
         }
     }
 
@@ -82,7 +86,6 @@ public class PlayerControllerEscape : MonoBehaviour {
         if (!fallen)
         {
             collision.gameObject.GetComponent<ObjectMoveScript>().Collision();
-            events.InvokeFallen();
             gameObject.transform.up = new Vector3(1, 0, 0);
             fallen = true;
             Rigidbody2D rBody = gameObject.GetComponent<Rigidbody2D>();
